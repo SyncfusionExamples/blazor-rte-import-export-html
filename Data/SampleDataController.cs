420570ec6c6767613d357f1d450e3b9fdfbf30ed
sathiskumarKR <sathiskumar.rajendran@syncfusion.com>
2023-11-30 17:42:33 +0530

    Sample(HTML): Create the Sample for Import and Export HTML files in Blazor RTE

---
+++ Data/SampleDataController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace RTE_Import_Export.Data
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleDataController : ControllerBase
+    {
+        private IWebHostEnvironment hostingEnv;
+
+        public SampleDataController(IWebHostEnvironment env)
+        {
+            this.hostingEnv = env;
+        }
+
+        [HttpPost]
+        [Route("Import")]
+        public void Import(IFormFile UploadFiles)
+        {
+            if (UploadFiles != null)
+            {
+                string htmlString;
+
+                using (StreamReader sr = new StreamReader(UploadFiles.OpenReadStream()))
+                {
+                    htmlString = sr.ReadToEnd();
+                }
+
+                string extract = ExtractBodyContent(htmlString);
+
+                string sanitizeString = SanitizeHtmlString(extract);
+
+                Response.Headers.Add("rtevalue", sanitizeString);
+            }
+        }
+
+        private string SanitizeHtmlString(string input)
+        {
+            input = Regex.Replace(input, @"[^\x20-\x7E]", "");
+            input = Regex.Replace(input, @"[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]", "");
+            input = input.Replace("\r", "").Replace("\n", "");
+            return input;
+        }
+
+        public string ExtractBodyContent(string html)
+        {
+            int bodyStart = html.IndexOf("<body>", StringComparison.OrdinalIgnoreCase);
+            int bodyEnd = html.IndexOf("</body>", StringComparison.OrdinalIgnoreCase);
+
+            if (bodyStart >= 0 && bodyEnd >= 0)
+            {
+                return html.Substring(bodyStart + 6, bodyEnd - bodyStart - 6);
+            }
+            return html;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using RTE_Import_Export.Data;
+using Syncfusion.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSyncfusionBlazor();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cWWdCf1JpR2ZGfV5yd0VDal5STnNYUiweQnxTdEZiWH5ZcHNURmRYWUxwWA==");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+app.MapControllers();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
